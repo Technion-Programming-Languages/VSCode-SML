579817f8756f0e701a1e402961c7c4ec996b8e11
Andrey Babyn <whitomtit@gmail.com>
2024-06-30 17:43:30 +0300

    Added hw3 and hw4 files

--- .devcontainer/fetch-git.sh
+++ .devcontainer/fetch-git.sh
@@ -3,4 +3,6 @@ cd hw_files
 git remote add -f origin https://github.com/OpenUniversity/PL-236319-Spr-2024.git
 git config core.sparseCheckout true 
 echo "Spr24/HW/Homework2/" > .git/info/sparse-checkout 
+echo "Spr24/HW/Homework3/" >> .git/info/sparse-checkout 
+echo "Spr24/HW/Homework4/" >> .git/info/sparse-checkout 
 git pull origin master
