13b335c26babc6491fb7445f645da69581962af6
Maroon Ayoub <maroonay@gmail.com>
2025-04-22 21:54:34 +0300

    synced to 2025

--- .devcontainer/fetch-git.sh
+++ .devcontainer/fetch-git.sh
@@ -1,10 +1,6 @@
-git config --global core.eol lf
-git config --global core.autocrlf input
 git init hw_files
 cd hw_files
-git remote add -f origin https://github.com/OpenUniversity/PL-236319-Spr-2024.git
+git remote add -f origin https://github.com/Technion-Programming-Languages/PL-236319-Spr-2025.git
 git config core.sparseCheckout true 
-echo "Spr24/HW/Homework2/" > .git/info/sparse-checkout 
-echo "Spr24/HW/Homework3/" >> .git/info/sparse-checkout 
-echo "Spr24/HW/Homework4/" >> .git/info/sparse-checkout 
+echo "Spr25/HW/Homework2/" > .git/info/sparse-checkout 
 git pull origin master
