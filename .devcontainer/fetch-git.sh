344bf21529496fa34f28517a7b2e7505b7f590fc
Maroon Ayoub <Maroonay@gmail.com>
2025-05-07 20:00:59 +0300

    Update fetch-git.sh

--- .devcontainer/fetch-git.sh
+++ .devcontainer/fetch-git.sh
@@ -2,5 +2,6 @@ git init hw_files
 cd hw_files
 git remote add -f origin https://github.com/Technion-Programming-Languages/PL-236319-Spr-2025.git
 git config core.sparseCheckout true 
-echo "Spr25/HW/Homework2/" > .git/info/sparse-checkout 
+echo "Spr25/HW/Homework2/" > .git/info/sparse-checkout
+echo "Spr25/HW/Homework3/" > .git/info/sparse-checkout 
 git pull origin master
